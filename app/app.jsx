427da95b7e27abd6dc5213dd64a1b4107b9e3a4b
rueda10 <rueda10@gmail.com>
2017-02-10 14:39:30 -0600

    Switched to SCSS

--- app/app.jsx
+++ app/app.jsx
@@ -15,7 +15,7 @@ require("style-loader!css-loader!foundation-sites/dist/css/foundation.min.css")
 $(document).foundation();
 
 // App css
-require("style-loader!css-loader!applicationStyles")
+require("style-loader!css-loader!sass-loader!applicationStyles")
 
 ReactDOM.render(
   <Router history={hashHistory}>
